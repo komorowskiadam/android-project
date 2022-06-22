442264e15d6a48a0a9923e60d4b2781a321a10b9
komortokozak <komorowski.adam2000@gmail.com>
2022-06-22 11:29:09 +0100

    pilisz

--- app/src/main/java/com/example/toogoodtogo/MainScreenActivity.kt
+++ app/src/main/java/com/example/toogoodtogo/MainScreenActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.os.StrictMode
 import android.view.View
 import android.widget.Button
+import android.widget.NumberPicker
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -21,9 +22,13 @@ class MainScreenActivity : AppCompatActivity(), OnDataPass {
     private var button: Button? = null
     private var button1: Button? = null
 
+    private var kmThreshold = 10
+
     private var packs = mutableListOf<Pack>()
 
     private var userId = "0"
+
+    private lateinit var numberPicker: NumberPicker
 
     private fun getPacks(){
         val url = URL("http://icm.dkkapusta1997.usermd.net/packages")
@@ -72,6 +77,37 @@ class MainScreenActivity : AppCompatActivity(), OnDataPass {
         // format - "$lat-$lng"
         // jak wysylasz request to /packages_coords/"$lat-$lng"/threshold
         // jesli zero
+
+        val url = URL("http://icm.dkkapusta1997.usermd.net/packages_coords/$userId/$data/$kmThreshold")
+        var fullstring = ""
+
+        val sdkVersion = Build.VERSION.SDK_INT
+        if (sdkVersion > 8) {
+            val policy = StrictMode.ThreadPolicy.Builder()
+                .permitAll().build()
+            StrictMode.setThreadPolicy(policy)
+            val conn = url.openConnection() as HttpURLConnection
+            conn.requestMethod = "GET"
+
+            BufferedReader(InputStreamReader(conn.inputStream)).use { br ->
+                var line: String?
+                while (br.readLine().also { line = it } != null) {
+                    fullstring += line
+                }
+            }
+        }
+        val gson = Gson()
+        val itemType = object : TypeToken<List<Pack>>() {}.type
+        try {
+            val itemList = gson.fromJson<List<Pack>>(fullstring, itemType)
+            packs = itemList as MutableList<Pack>
+            val adapter = PackAdapter(packs, false, userId)
+            val recyclerView = findViewById<RecyclerView>(R.id.pack_list)
+            recyclerView.layoutManager = LinearLayoutManager(this)
+            recyclerView.adapter = adapter
+        } catch (e: Exception){
+
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -80,6 +116,15 @@ class MainScreenActivity : AppCompatActivity(), OnDataPass {
 
         intent = intent
         userId = intent.getStringExtra(UserAdapter.EXTRA_USER_ID).toString()
+
+        numberPicker = findViewById(R.id.numberPicker)
+        numberPicker.minValue = 0
+        numberPicker.maxValue = 100
+        numberPicker.value = kmThreshold
+
+        numberPicker.setOnValueChangedListener { _, _, newVal ->
+            kmThreshold = newVal
+        }
 
         val userPacksButton = findViewById<Button>(R.id.users_packs_btn)
         userPacksButton.setOnClickListener {
